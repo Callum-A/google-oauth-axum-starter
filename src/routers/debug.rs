5e8b1c9337e2348efe1fd2d5ae4f241f334cca69
Callum <46447937+Callum-A@users.noreply.github.com>
2024-01-28 19:02:28 +0000

    Add explicit status codes, user table DDL (#3)

--- src/routers/debug.rs
+++ src/routers/debug.rs
@@ -1,15 +1,16 @@
-use axum::extract::State;
+use axum::{extract::State, http::StatusCode};
 use sqlx::Error;
 
 use crate::state::app::AppState;
 
-pub async fn health_check(State(state): State<AppState>) -> &'static str {
+pub async fn health_check(State(state): State<AppState>) -> (StatusCode, &'static str) {
     let row: Result<(i32,), Error> = sqlx::query_as("SELECT $1")
         .bind(1_i32)
         .fetch_one(&state.pool)
         .await;
+    tracing::info!("Event=HealthCheck row={:?}", row);
     match row {
-        Ok(_) => "OK",
-        Err(_) => "NOK",
+        Ok(_) => (StatusCode::OK, "OK"),
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "NOK"),
     }
 }
--- src/routers/oauth.rs
+++ src/routers/oauth.rs
@@ -1,5 +1,6 @@
 use axum::{
     extract::{Query, State},
+    http::StatusCode,
     Json,
 };
 use serde::Deserialize;
@@ -11,21 +12,30 @@ pub struct GoogleOAuth {
     code: String,
 }
 
-pub async fn google_oauth(oauth: Query<GoogleOAuth>, State(state): State<AppState>) -> Json<User> {
+pub async fn google_oauth(
+    oauth: Query<GoogleOAuth>,
+    State(state): State<AppState>,
+) -> (StatusCode, Json<Option<User>>) {
     let details = state
         .google_oauth_client
         .perform_google_oauth(&oauth.code)
-        .await
-        .unwrap();
+        .await;
+
+    if let Err(e) = details {
+        tracing::error!("Event=GoogleOAuthError error={:?}", e);
+        return (StatusCode::INTERNAL_SERVER_ERROR, Json(None));
+    }
+
+    let details = details.unwrap();
     let user = User::from(details);
     let existing_user = state.user_repository.find_by_email(&user.email).await;
 
     if let Some(user) = existing_user {
         tracing::info!("Event=ExistingUser user={:?}", user);
-        return Json(user);
+        return (StatusCode::OK, Json(Some(user)));
     }
 
     state.user_repository.create_user(&user).await;
     tracing::info!("Event=CreatedUser user={:?}", user);
-    Json(user)
+    (StatusCode::OK, Json(Some(user)))
 }
